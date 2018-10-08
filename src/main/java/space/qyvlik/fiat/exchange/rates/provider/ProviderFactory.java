001fb149f5c0e8102dd4a3a8795305663d96b2ad
qyvlik <qyvlik@qq.com>
2018-10-08 13:57:01 +0800

    add european central bank

--- src/main/java/space/qyvlik/fiat/exchange/rates/provider/ProviderFactory.java
+++ src/main/java/space/qyvlik/fiat/exchange/rates/provider/ProviderFactory.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import space.qyvlik.fiat.exchange.rates.base.AbstractFiatExchangeRatesProvider;
 import space.qyvlik.fiat.exchange.rates.base.BaseService;
 import space.qyvlik.fiat.exchange.rates.provider.currencylayer.CurrencyLayerProvider;
+import space.qyvlik.fiat.exchange.rates.provider.europeancentralbank.EuropeanCentralBankProvider;
 import space.qyvlik.fiat.exchange.rates.provider.oneforge.OneForgeProvider;
 
 import javax.annotation.PostConstruct;
@@ -23,10 +24,14 @@ public class ProviderFactory extends BaseService {
     @Autowired
     private OneForgeProvider oneForgeProvider;
 
+    @Autowired
+    private EuropeanCentralBankProvider europeanCentralBankProvider;
+
     @PostConstruct
     protected void init() {
         providerMap.put(currencyLayerProvider.getProvider(), currencyLayerProvider);
         providerMap.put(oneForgeProvider.getProvider(), oneForgeProvider);
+        providerMap.put(europeanCentralBankProvider.getProvider(), europeanCentralBankProvider);
     }
 
     public List<AbstractFiatExchangeRatesProvider> providerList() {
